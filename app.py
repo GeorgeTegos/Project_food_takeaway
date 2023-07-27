9a40c3f1e0c8ba523af9bfa78aca738c60191bfd
George Tegos <george.tegos.22@gmail.com>
2023-07-27 19:39:00 +0100

    fixed seed feature

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user@localhost:5432/food_takeaway_project"
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
-# from seed import seed
-# app.cli.add_command(seed)
+from seed import seed
+app.cli.add_command(seed)
 
 from controllers.item_controller import item_blueprint
 from controllers.order_controller import order_blueprint
