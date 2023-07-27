4a47ca32e9683f1fee947db0bffd32791e7896f5
George Tegos <george.tegos.22@gmail.com>
2023-07-27 16:49:11 +0100

    setup Models

--- models.py
+++ models.py
@@ -1,7 +1,20 @@
-class Order():
+from app import db
 
-    pass
+class Order():
+    __tablename__="order"
+
+    id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String(64))
+    customer_phone = db.Column(db.Integer)
+    customer_address = db.Column(db.String(64))
+    order_delivered = db.Column(db.Boolean, default = False)
+
 
 class Item():
 
-    pass
+    __table__="item"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(64))
+    item_price = db.Column(db.Integer)
+    
