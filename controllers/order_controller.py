07f605dccec7e0d5d378f0d07b2077aee8248eca
George Tegos <george.tegos.22@gmail.com>
2023-07-30 19:51:10 +0100

    css and edit order

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -32,10 +32,29 @@ def send_new_order():
     name = request.form['customer_name']
     phone = request.form['customer_phone']
     address = request.form['customer_address']
-
     new_order = Order(customer_name=name, customer_phone=phone, customer_address=address)
-
-
     db.session.add(new_order)
     db.session.commit()
     return redirect('/')
+
+@order_blueprint.route('/orders/<id>/edit')
+def edit_order(id):
+    order = Order.query.get(id)
+    items = Item.query.join(Order_Item).filter(Order_Item.order_id == id)
+    phone= int(order.customer_phone)
+    return render_template('orders/edit.jinja',order=order,items=items,phone=phone)
+
+@order_blueprint.route("/orders/<id>" , methods=["post"])
+def confirm_edit(id):
+
+    name = request.form['customer_name']
+    phone = request.form['customer_phone']
+    address = request.form['customer_address']
+    order = Order.query.get(id)
+
+    order.customer_name = name
+    order.customer_phone = phone
+    order.customer_address = address
+
+    db.session.commit()
+    return redirect('/orders')
