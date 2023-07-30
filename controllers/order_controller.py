60d46c518427c4f96fd110d484d55b168b34d8b8
George Tegos <george.tegos.22@gmail.com>
2023-07-30 13:31:17 +0100

    new order (no item)

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -12,17 +12,30 @@ def show_all_orders():
     orders = Order.query.all()
     return render_template('orders/orders.jinja',orders=orders)
 
-# @order_blueprint.route('/orders/<id>')
-# def show_order_by_id(id):
-#     order = Order.query.get(id)
-#     phone= int(order.customer_phone)
-#     return render_template("orders/show.jinja",order=order,phone=phone)
-
 @order_blueprint.route('/orders/<id>')
 def show_by_id(id):
     order = Order.query.get(id)
     items = Item.query.join(Order_Item).filter(Order_Item.order_id == id)
+    phone= int(order.customer_phone)
     total = 0
     for item in items:
         total += item.item_price
-    return render_template('orders/show.jinja',order=order,items=items,total=total)
+    return render_template('orders/show.jinja',order=order,items=items,total=total,phone=phone)
+
+@order_blueprint.route('/orders/new')
+def make_new_order():
+    items = Item.query.all()
+    return render_template('orders/new_order.jinja',items=items)
+
+@order_blueprint.route('/orders', methods=['POST'])
+def send_new_order():
+    name = request.form['customer_name']
+    phone = request.form['customer_phone']
+    address = request.form['customer_address']
+
+    new_order = Order(customer_name=name, customer_phone=phone, customer_address=address)
+
+
+    db.session.add(new_order)
+    db.session.commit()
+    return redirect('/')
