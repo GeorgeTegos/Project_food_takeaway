ebabaac8348522aea8d9091307e83bb7c069a31b
George Tegos <george.tegos.22@gmail.com>
2023-08-02 14:43:09 +0100

    switched INT to FLOAT price

--- models/Item.py
+++ models/Item.py
@@ -5,7 +5,7 @@ class Item(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     item_name = db.Column(db.String(64))
-    item_price = db.Column(db.Integer)
+    item_price = db.Column(db.Float)
     order_item = db.relationship("Order_Item", backref="item", cascade="all, delete-orphan")
 
     def __repr__(self):
